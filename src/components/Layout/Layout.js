d7c57b6278ab7486c1b3726fabb23392853454b2
Jay Fallon <jayfallon@gmail.com>
2019-08-15 16:06:46 -0400

    added hamburger button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ import SideDrawer from "../Navigation/SideDrawer/SideDraw";
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   };
 
   sideDrawerClosedHandler = () => {
