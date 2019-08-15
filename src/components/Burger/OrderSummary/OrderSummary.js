939336ad9d4cb28021877b56079476729e0e9d1e
Jay Fallon <jayfallon@gmail.com>
2019-08-15 11:56:43 -0400

    added total price to modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -14,6 +14,9 @@ const orderSummary = props => {
       <h3>Your order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total price:</strong> {props.price}
+      </p>
       <p>Continue to checkout?</p>
       <Button clicked={props.purchaseCancelled} btnType={"Danger"}>
         CANCEL
